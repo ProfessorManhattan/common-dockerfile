4cdabd359d446eb91ccb49e79a23ea8790415935
Brian Zalewski <brian@enviedsolutions.com>
2021-04-15 16:15:51 +0000

    Update README.md, initctl, files/.gitlab-ci.yml, files/LICENSE, files/package.json, update.sh files

---
+++ update.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# This script performs maintenance on this repository. It ensures git submodules are
+# installed and then copies over base files from the modules. It also generates the
+# documentation.
+
+set -ex
+
+# Ensure submodules are present and up-to-date
+if [ ! -d "./.modules/docs" ]; then
+  git submodule add -b master https://gitlab.com/megabyte-space/documentation/dockerfile.git ./.modules/docs
+else
+  cd ./.modules/docs
+  git checkout master && git pull origin master
+  cd ../..
+fi
+if [ ! -d "./.modules/shared" ]; then
+  git submodule add -b master https://gitlab.com/megabyte-space/common/shared.git ./.modules/shared
+else
+  cd ./.modules/shared
+  git checkout master && git pull origin master
+  cd ../..
+fi
+
+# Copy over files from the shared submodule
+cp -Rf ./.modules/shared/.github .
+cp -Rf ./.modules/shared/.gitlab .
+cp -Rf ./.modules/shared/.vscode .
+cp ./.modules/shared/.editorconfig .editorconfig
+cp ./.modules/shared/CODE_OF_CONDUCT.md CODE_OF_CONDUCT.md
+
+# Copy files over from the Dockerfile shared submodule
+if [ -f ./package.json ]; then
+  # Retain package.json "name", "description", and "version"
+  PACKAGE_DESCRIPTION=$(cat package.json | jq '.description')
+  PACKAGE_NAME=$(cat package.json | jq '.name' | cut -d '"' -f 2)
+  PACKAGE_VERSION=$(cat package.json | jq '.version' | cut -d '"' -f 2)
+  cp -Rf ./.modules/dockerfile/files/ .
+  jq --arg a ${PACKAGE_DESCRIPTION//\/} '.description = $a' package.json > __jq.json && mv __jq.json package.json
+  jq --arg a ${PACKAGE_NAME//\/} '.name = $a' package.json > __jq.json && mv __jq.json package.json
+  jq --arg a ${PACKAGE_VERSION//\/} '.version = $a' package.json > __jq.json && mv __jq.json package.json
+  npx prettier-package-json --write
+else
+  cp -Rf ./.modules/dockerfile/files/ .
+fi
+
+# Ensure the pre-commit hook is executable
+chmod 755 .husky/pre-commit
+
+# Run logic unique to the container's OS
+DOCKERFILE_FIRSTLINE=$(head -n 1 ./Dockerfile)
+if [[ "$DOCKERFILE_FIRSTLINE" == *"ubuntu"* ]]; then
+  cp ./.modules/dockerfile/initctl initctl
+fi
+if [[ "$DOCKERFILE_FIRSTLINE" == *"debian"* ]]; then
+  cp ./.modules/dockerfile/initctl initctl
+fi
+
+# Generate the documentation
+jq -s '.[0] * .[1]' .blueprint.json ./.modules/docs/common.json > __bp.json | true
+npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-contributing.md --output CONTRIBUTING.md | true
+npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-readme.md | true
+rm __bp.json
+
+echo "*** Done updating meta files and generating documentation ***"
