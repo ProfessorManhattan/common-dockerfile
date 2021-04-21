3b85482f4b75295d66b1d716e83e35539a70c5e2
Brian Zalewski <brian@megabyte.space>
2021-04-21 02:44:53 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -32,13 +32,15 @@ cp ./.modules/shared/CODE_OF_CONDUCT.md CODE_OF_CONDUCT.md
 # Copy files over from the Dockerfile shared submodule
 if [ -f ./package.json ]; then
   # Retain package.json "name", "description", and "version"
-  PACKAGE_DESCRIPTION=$(cat package.json | jq '.description')
+  PACKAGE_DESCRIPTION=$(cat package.json | jq '.description' | cut -d '"' -f 2)
   PACKAGE_NAME=$(cat package.json | jq '.name' | cut -d '"' -f 2)
   PACKAGE_VERSION=$(cat package.json | jq '.version' | cut -d '"' -f 2)
+  PACKAGE_SLIM_BUILD=$(cat package.json | jq '.scripts."build:slim"' | cut -d '"' -f 2)
   cp -Rf ./.modules/dockerfile/files/ .
   jq --arg a ${PACKAGE_DESCRIPTION//\/} '.description = $a' package.json > __jq.json && mv __jq.json package.json
   jq --arg a ${PACKAGE_NAME//\/} '.name = $a' package.json > __jq.json && mv __jq.json package.json
   jq --arg a ${PACKAGE_VERSION//\/} '.version = $a' package.json > __jq.json && mv __jq.json package.json
+  jq --arg a ${PACKAGE_SLIM_BUILD//\/} '.scripts."build:slim" = $a' package.json > __jq.json && mv __jq.json package.json
   npx prettier-package-json --write
 else
   cp -Rf ./.modules/dockerfile/files/ .
@@ -47,19 +49,29 @@ fi
 # Ensure the pre-commit hook is executable
 chmod 755 .husky/pre-commit
 
-# Run logic unique to the container's OS
-DOCKERFILE_FIRSTLINE=$(head -n 1 ./Dockerfile)
-if [[ "$DOCKERFILE_FIRSTLINE" == *"ubuntu"* ]]; then
-  cp ./.modules/dockerfile/initctl initctl
+# Determine type of Dockerfile project
+DOCKERFILE_PROJECT_TYPE=$(cat .blueprint.json | jq '.subgroup' | cut -d '"' -f 2)
+if [ "$DOCKERFILE_PROJECT_TYPE" == "ansible-molecule" ]; then
+  # Run logic unique to the container's OS
+  DOCKERFILE_FIRSTLINE=$(head -n 1 ./Dockerfile)
+  if [[ "$DOCKERFILE_FIRSTLINE" == *"ubuntu"* ]]; then
+    cp ./.modules/dockerfile/initctl initctl
+  fi
+  if [[ "$DOCKERFILE_FIRSTLINE" == *"debian"* ]]; then
+    cp ./.modules/dockerfile/initctl initctl
+  fi
 fi
-if [[ "$DOCKERFILE_FIRSTLINE" == *"debian"* ]]; then
-  cp ./.modules/dockerfile/initctl initctl
+
+# Handle Dockerfile project-type specific README.md template reference
+BLUEPRINT_README_FILE="blueprint-readme.md"
+if [ "$DOCKERFILE_PROJECT_TYPE" == "ansible-molecule" ]; then
+  BLUEPRINT_README_FILE="blueprint-readme-ansible-molecule.md"
 fi
 
 # Generate the documentation
 jq -s '.[0] * .[1]' .blueprint.json ./.modules/docs/common.json > __bp.json | true
 npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-contributing.md --output CONTRIBUTING.md | true
-npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-readme.md | true
+npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/$BLUEPRINT_README_FILE | true
 rm __bp.json
 
 echo "*** Done updating meta files and generating documentation ***"
