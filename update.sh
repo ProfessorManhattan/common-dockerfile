ccbfad8625c74477ac59283c550ab0016d434b24
Brian Zalewski <brian@megabyte.space>
2021-04-27 00:46:58 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -86,7 +86,7 @@ rm __bp.json
 
 if [ "$DOCKERFILE_PROJECT_TYPE" == "ci-pipeline" ]; then
   PACKAGE_SLIM_BUILD=$(cat package.json | jq '.scripts."build:slim"' | cut -d '"' -f 2)
-  sed -i .bak "s/DOCKER_SLIM_BUILD_COMMAND/${PACKAGE_SLIM_BUILD}/g" README.md
+  sed -i '.bak' "s^DOCKER_SLIM_BUILD_COMMAND^${PACKAGE_SLIM_BUILD}^g" README.md
 fi
 
 # Remove formatting error
