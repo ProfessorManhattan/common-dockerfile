78e453e3450500e0f9428ff1602f33a0bca6de58
Brian Zalewski <brian@megabyte.space>
2021-05-14 01:49:42 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ set -e
 # shellcheck disable=SC1091
 source "./.modules/shared/update.lib.sh"
 
-if [[ "$(docker images -q megabytelabs/DOCKERFILE_SLUG:slim 2> /dev/null)" == "" ]] || [[ "$(docker images -q megabytelabs/DOCKERFILE_SLUG:latest 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q megabytelabs/DOCKER_SLUG:slim 2> /dev/null)" == "" ]] || [[ "$(docker images -q megabytelabs/DOCKER_SLUG:latest 2> /dev/null)" == "" ]]; then
   info "The images have not been built yet"
   log "Running 'npm run build'"
   npm run build
@@ -18,9 +18,9 @@ fi
 
 for TEST_SCENARIO in test/*/; do
   log "Running the regular image and capturing the output for the ${TEST_SCENARIO} scenario"
-  LATEST_OUTPUT=$(docker run -v "${PWD}:/work/${TEST_SCENARIO}" -w /work megabytelabs/DOCKERFILE_SLUG:latest DOCKERFILE_COMMAND)
+  LATEST_OUTPUT=$(docker run -v "${PWD}:/work/${TEST_SCENARIO}" -w /work megabytelabs/DOCKER_SLUG:latest DOCKER_COMMAND)
   log "Running the slim image and capturing the output for the ${TEST_SCENARIO} scenario"
-  SLIM_OUTPUT=$(docker run -v "${PWD}:/work/${TEST_SCENARIO}" -w /work megabytelabs/DOCKERFILE_SLUG:slim DOCKERFILE_COMMAND)
+  SLIM_OUTPUT=$(docker run -v "${PWD}:/work/${TEST_SCENARIO}" -w /work megabytelabs/DOCKER_SLUG:slim DOCKER_COMMAND)
   log "Comparing the output from the regular image and slim image for the ${TEST_SCENARIO} scenario"
   if [ "$LATEST_OUTPUT" == "$SLIM_OUTPUT" ]; then
     success "The output from the slim image matches the output from the regular image for the ${TEST_SCENARIO} scenario"
