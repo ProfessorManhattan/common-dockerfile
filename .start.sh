7cf297debc3d7984fb52ff895174d31b10bb6968
Brian Zalewski <brian@megabyte.space>
2021-07-19 16:46:01 +0000

    Update files/.husky/pre-commit, files/.gitignore, files/requirements.txt, files/.vscode/extensions.json, files/.vscode/tasks.json, files-ansible-molecule/.gitkeep, files-app/.gitkeep, files-ci-pipeline/.gitkeep, files-software/.gitkeep, taskfiles/Taskfile-ansible-molecule.yml.handlebars, taskfiles/Taskfile-apps.yml.handlebars, taskfiles/Taskfile-ci-pipeline.yml.handlebars, taskfiles/Taskfile-dockerfile.yml.handlebars, .start.sh files Deleted files-*/.github/FUNDING.yml, files-*/.github/ISSUE_TEMPLATE.md, files-*/.github/PULL_REQUEST_TEMPLATE.md, files-*/.github/stale.yml, files-*/.gitlab/issue_templates/Defect.md, files-*/.gitlab/issue_templates/Feature.md, files-*/.gitlab/issue_templates/Testing.md, files-*/.husky/.gitignore, files-*/.husky/pre-commit, files-*/.editorconfig, files-*/.gitignore, files-*/.gitlab-ci.yml, files-*/.prettierignore, files-*/CODE_OF_CONDUCT.md, files-*/LICENSE, files-*/common.json, files-*/package.json, files-*/requirements.txt, files/LICENSE, files/CODE_OF_CONDUCT.md, taskfiles/Taskfile-darwin.yml, taskfiles/Taskfile-common.yml, taskfiles/Taskfile-linux.yml, taskfiles/Taskfile-software.yml, taskfiles/Taskfile-windows.yml files

--- .start.sh
+++ .start.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# This script performs maintenance on this repository. It ensures git submodules are
-# installed and then copies over base files from the modules. It also generates the
-# documentation.
+# @file .start.sh
+# @brief Ensures the project is up-to-date with the latest upstream changes
+# @description
+#   This script performs maintenance on this repository. It ensures the `.common` submodule is
+#   cloned and up-to-date. The `.common` submodule contains files that are shared between projects
+#   that are similar to this one. It also ensures the repository contains the latest code. This
+#   script calls the script in `.common/update.sh` which will make sure the project is
+#   bootstrapped. `bash .start.sh` is the first command you should run when working with this project.
 
 set -e
-
-# shellcheck disable=SC2154
-if [ "$container" != 'docker' ]; then
-  curl -sL https://git.io/_has | bash -s docker git jq node npm wget
-fi
-
 export REPO_TYPE=dockerfile
-git pull origin master --ff-only
-git submodule update --init --recursive
-if [ ! -f "./.modules/${REPO_TYPE}/update.sh" ]; then
-  mkdir -p ./.modules || exit
-  git submodule add -b master https://gitlab.com/megabyte-space/common/$REPO_TYPE.git ./.modules/$REPO_TYPE
-else
-  cd ./.modules/$REPO_TYPE || exit
-  git checkout master && git pull origin master --ff-only
-  cd ../.. || exit
+
+if [ -d .git ]; then
+  git pull origin master --ff-only
+  git submodule update --init --recursive
 fi
-bash ./.modules/$REPO_TYPE/update.sh
+
+if [ ! -d .common ]; then
+  git submodule add -b master "https://gitlab.com/megabyte-space/common/$REPO_TYPE.git" ".common"
+else
+  cd .common
+  git checkout master && git pull origin master --ff-only
+  cd ..
+fi
+
+bash .common/update.sh
